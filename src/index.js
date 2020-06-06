ab815eeffe126c9e9aa8b6c0ebffd2f548d61b2c
Eugene <yevheniiba@wix.com>
2020-06-06 18:08:57 +0300

    Upgrade dependencies

--- src/index.js
+++ src/index.js
@@ -2,16 +2,7 @@ const unified = require('unified');
 const parse = require('remark-parse');
 const slackify = require('./slackify');
 
-const defaultOptions = {
-  pedantic: true,
-};
-
 module.exports = (markdown, options) => {
-  options = {
-    ...defaultOptions,
-    ...options,
-  };
-
   return unified()
     .use(parse, options)
     .use(slackify)
